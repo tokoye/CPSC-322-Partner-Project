6390ce3191a7ccbaa5d009a8a022d389323a4f47
cwalk-13 <59989219+cwalk-13@users.noreply.github.com>
2021-05-04 20:38:13 -0700

    som

--- mysklearn/myutils.py
+++ mysklearn/myutils.py
@@ -31,7 +31,7 @@ import mysklearn.myevaluation as myevaluation
 
 def get_col(mypy, col_name):
     return mypy.get_column(col_name, False)
-    
+
 def get_column(table, header, col_name):
     col_index = header.index(col_name)
     col = []
@@ -634,7 +634,11 @@ def best_M(M, d):# returns M biggest values from list
 
     return best
 
-
+def compute_corr_coef(x, y):
+    mean_x = np.mean(x)
+    mean_y = np.mean(y)
+    r = sum([(x[i] - mean_x) * (y[i] - mean_y) for i in range(len(x))]) / sum([((x[i] - mean_x)**2) * ((y[i] - mean_y)**2) for i in range(len(x))])
+    return r
 
 def bagging(X,Y,N,M,F):
 # 1. split your dataset into a test set and a "remainder set"
